f3fb6523b0fcd4f498856deb1e21aeeaca64b7a6
zarin-anjum <zarinanjum0801@gmail.com>
2025-09-04 15:52:36 +0600

    call history work in progress

--- script.js
+++ script.js
@@ -4,9 +4,26 @@ function setupCounter(buttons, display){
             let count = parseInt(display.textContent || 0);
             display.textContent = count + 1;
             let clicked = true;
+
+            const card = btn.closest('.card');
+            const cardName = card.querySelector('.card-title').textContent;
+
+            alert(`Copied from: ${cardName}`);
         }); 
     });
 }
 
 setupCounter(document.querySelectorAll('.btn-heart'), document.getElementById('navbar-heart-count'));
 setupCounter(document.querySelectorAll('.btn-copy'), document.getElementById('copy-count'));
+
+const coinDisplay = document.getElementById('coin-count');
+let coins = parseInt(coinDisplay.textContent) || 100;
+
+const callBtn = document.querySelectorAll('.btn-call');
+const callHistoryList = document.getElementById('call-history-list');
+
+callBtn.forEach(btn => {
+    const card = btn.closest('.bg-white.rounded-xl.shadow-md');
+    const serviceName = card.querySelector('h3.font-hind').textContent;
+    const serviceNumber = card.querySelector('h2').textContent;
+})
